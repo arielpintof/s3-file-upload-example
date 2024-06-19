00f3373a00b04dec90eb1f90e334eb176df5b069
Ariel <ariel.pintof@userena.cl>
2024-06-19 18:22:10 -0400

    typo

--- src/app/(form)/actions.ts
+++ src/app/(form)/actions.ts
@@ -2,11 +2,12 @@
 
 import { revalidatePath } from "next/cache";
 import { validateFormData } from "./validation";
+
 interface Message{
     status: string;
     message: string;
-
 }
+
 export async function uploadFile(prevState : any, formData : FormData) : Promise<Message> {
     const validation = await validateFormData(formData);
 
