457605295f94e65ac45f9c584eae87922d64b991
Ariel <ariel.pintof@userena.cl>
2024-06-19 22:47:59 -0400

    add s3 client

--- src/app/(form)/actions.ts
+++ src/app/(form)/actions.ts
@@ -2,11 +2,27 @@
 
 import { revalidatePath } from "next/cache";
 import { validateFormData } from "./validation";
+import { S3Client, PutObjectCommand} from "@aws-sdk/client-s3"
 
 interface Message{
     status: string;
     message: string;
 }
+
+const accessKeyId = process.env.NEXT_AWS_S3_ACCESS_KEY_ID;
+const secretAccessKey = process.env.NEXT_AWS_S3_SECRET_ACCESS_KEY;
+
+if (!accessKeyId || !secretAccessKey) {
+    throw new Error("Missing AWS credentials");
+}
+
+const s3Client = new S3Client({
+    region: process.env.NEXT_AWS_S3_REGION,
+    credentials: {
+        accessKeyId,
+        secretAccessKey
+    },
+});
 
 export async function uploadFile(prevState : any, formData : FormData) : Promise<Message> {
     const validation = await validateFormData(formData);
@@ -16,14 +32,37 @@ export async function uploadFile(prevState : any, formData : FormData) : Promise
     }
 
     try {
-        console.log(formData);
+        const file = formData.get("file") as File;
+        const buffer = Buffer.from(await file.arrayBuffer());
+        await uploadFileToS3(file.name, buffer);
         revalidatePath("/");
 
         return { status: "success", message: "File uploaded successfully!" };
 
     } catch (error) {
-        console.error(error);
+        
         return { status: "error", message: "Error uploading file" };
+    }
+}
+
+async function uploadFileToS3(fileName: string, buffer: Buffer) {
+    const fileBuffer = buffer;
+
+    const params = {
+        Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
+        Key: fileName,
+        Body: buffer,
+    }
+    
+    const command = new PutObjectCommand(params);
+
+    try{
+        const response = await s3Client.send(command);
+        console.log(response);
+
+        return fileName
+    } catch(error){
+        console.error(error);
     }
 }
 
